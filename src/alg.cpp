916add554e78e49c1ea238dc8c497ec827c68915
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-03-17 16:50:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,26 +1,24 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int* arr, int len, int value)
-{
+int countPairs1(int* arr, int len, int value) {
 int count = 0;
-for (int i = 0; i < len-1; i++) {
+for (int i = 0; i < len - 1; i++) {
   for (int j = i + 1; j < len; j++) {
-	  if ((arr[i] +arr[j]) == value) {
-		  count += 1;	
-	  }
+    if ((arr[i] + arr[j]) == value) {
+      count += 1;	
+    }
   }
 }
 return count;
 }
-int countPairs2(int* arr, int len, int value)
-{
+int countPairs2(int* arr, int len, int value) {
 int count = 0;
-for (int i = 0; i < len-1; i++) {
+for (int i = 0; i < len - 1; i++) {
   if (arr[i] < value) {
-	  for (int j = len-1; j > i; j--) {
-		  if (arr[j] < value && ((arr[i] + arr[j]) == value)) {
-			  count += 1;
-		  }
-	  }
+    for (int j = len - 1; j > i; j--) {
+      if (arr[j] < value && ((arr[i] + arr[j]) == value)) {
+        count += 1;
+      }
+    }
   }
 }
 return count;
@@ -29,35 +27,33 @@ int sort(int* arr, int len) {
 int a = 0;
 for (int i = 0; i < len; i++) {
   for (int j = 0; j < len - 1; j++) {
-	  if (arr[j] > arr[j + 1]) {
-		  a = arr[j];
-		  arr[j] = arr[j + 1];
-		  arr[j + 1] = a;
-		  }
-	  }
+  if (arr[j] > arr[j + 1]) {
+    a = arr[j];
+    arr[j] = arr[j + 1];
+    arr[j + 1] = a;
+    }
+  }
 }
 return *arr;
 }
-int countPairs3(int* arr, int len, int value)
-{
+int countPairs3(int* arr, int len, int value) {
 sort(arr, len);
-int mid=0, count=0;
+int mid = 0, count = 0;
 int left = 0, right = 0;
 for (int i = 0; i < len; i++) {
   left = i + 1 , right = len;	
   while (left < right) {
-	  mid = (left + right) / 2;
-	  if (arr[mid] < (value - arr[i])) {
-	    left = mid + 1;
-}
-	  else {
+mid = (left + right) / 2;
+  if (arr[mid] < (value - arr[i])) {
+    left = mid + 1;
+} else {
     right = mid;
   }
 }
 while (arr[left] == (value - arr[i])) {
   count+=1;
-  left+=1;	
-  }	
+  left+=1;
+  }
 }
 return count;
 }
