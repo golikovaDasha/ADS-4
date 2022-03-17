510425c810f8ff3e73441de78488b7f6170a0380
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-03-17 17:31:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,18 +23,17 @@ for (int i = 0; i < len - 1; i++) {
 }
 return count;
 }
-int sort(int* arr, int len) {
-int a = 0;
-for (int i = 0; i < len; i++) {
-  for (int j = 0; j < len - 1; j++) {
-  if (arr[j] > arr[j + 1]) {
-    a = arr[j];
-    arr[j] = arr[j + 1];
-    arr[j + 1] = a;
-    }
+void sort(int* arr, int len) {
+int temp,j;
+for (int i = 1; i < len; i++) {
+  j = i;
+  temp = arr[i];
+  while (j > 0 && temp < arr[j - 1]) {
+    arr[j] = arr[j - 1];
+    j--;
   }
-}
-return *arr;
+  arr[j] = temp;
+  }	
 }
 int countPairs3(int* arr, int len, int value) {
 sort(arr, len);
